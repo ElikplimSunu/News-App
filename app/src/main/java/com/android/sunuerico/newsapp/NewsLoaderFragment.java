049a667799b80d0adb49541f43b0bee2e9b11c0c
Sunu Eric <sunuerico@gmail.com>
2020-06-22 00:33:01 +0000

    Initial commit

---
+++ app/src/main/java/com/android/sunuerico/newsapp/NewsLoaderFragment.java
@@ -0,0 +1,50 @@
+package com.android.sunuerico.newsapp;
+
+import android.content.Context;
+import android.os.AsyncTask;
+
+import androidx.loader.content.AsyncTaskLoader;
+
+import com.android.sunuerico.newsapp.Helpers.QueryUtilities;
+import com.android.sunuerico.newsapp.Objects.News;
+
+import java.util.List;
+
+public class NewsLoaderFragment extends AsyncTaskLoader<List<News>>{
+	private String mUrl;
+
+	/**
+	 * NewsLoaderFragment constructor
+	 *
+	 * @param context  context of app
+	 * @param queryUrl url string as per user input in SearchView
+	 */
+	public NewsLoaderFragment(Context context, String queryUrl) {
+		super(context);
+		mUrl = queryUrl;
+	}
+
+	@Override
+	protected void onStartLoading() {
+		super.onStartLoading();
+		forceLoad();
+	}
+
+	/**
+	 * This method is called to load data from url string in background thread
+	 *
+	 * @return News detail in form of List<News>
+	 */
+	@Override
+	public List<News> loadInBackground() {
+
+		// If url string is null return null object
+		if (mUrl == null) {
+			return null;
+		}
+
+		// Get the List<News> from URL string queried by user
+		List<News> newsData = QueryUtilities.fetchNewsData(mUrl);
+		return newsData;
+	}
+}
---
+++ app/src/main/java/com/android/sunuerico/newsapp/Values/Keys.java
@@ -0,0 +1,20 @@
+package com.android.sunuerico.newsapp.Values;
+
+public class Keys {
+
+	// Keys for extracting JSON object
+	public static final String JSON_NEWS_OBJECT_KEY = "response";
+	public static final String JSON_NEWS_ARRAY_KEY = "results";
+	public static final String JSON_NEWS_SECTION_KEY = "sectionName";
+	public static final String JSON_NEWS_HEADLINE_KEY = "webTitle";
+	public static final String JSON_NEWS_PUBLISHED_TIME_KEY = "webPublicationDate";
+	public static final String JSON_NEWS_URL_KEY = "webUrl";
+	public static final String JSON_NEWS_FIELDS_KEY = "fields";
+	public static final String JSON_NEWS_THUMBNAIL_KEY = "thumbnail";
+	public static final String JSON_NEWS_TAGS_KEY = "tags";
+	public static final String JSON_NEWS_AUTHOR_NAME_KEY = "webTitle";
+
+	Keys() {
+		// Empty constructor so it cannot be used mistakenly
+	}
+}
